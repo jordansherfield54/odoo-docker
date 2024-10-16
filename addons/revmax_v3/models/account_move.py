a1cf4d06e08209b0e2c9eb9946e49569bf4e7ed1
jordansherfield54 <121431577+jordansherfield54@users.noreply.github.com>
2024-10-16 17:03:53 +0200

    Update account_move.py

--- addons/revmax_v3/models/account_move.py
+++ addons/revmax_v3/models/account_move.py
@@ -251,7 +251,7 @@ class AccountMove(models.Model):
 
                     myobj = {
                         "Currency": str(record.currency_id.name),
-                        "BranchName": "Bridge SPAR",
+                        "BranchName": "Main",
                         "InvoiceNumber": str(record.name),
                         "CustomerName": CustomerName,
                         "CustomerVatNumber": CustomerVatNumber,
@@ -276,7 +276,7 @@ class AccountMove(models.Model):
                     _logger.info(myobj)
 
                     # Post the data to revmax
-                    x = requests.post(url, headers=headers, data=myobj, timeout=30)
+                    x = requests.post(url, headers=headers, data=myobj, timeout=90)
 
                     data = x.json()
                     log = json.dumps(
